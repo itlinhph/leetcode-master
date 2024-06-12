2fd94b7186fbb676bcb98ffcd5ae9198612a682b
hieubz <phamduyhieuit@gmail.com>
2024-06-12 13:52:30 +0700

    remove duplicates from sorted array

---
+++ hieupd/src/main/java/com/neetcode/two_pointers/RemoveDuplicateFromSortedArray.java
@@ -0,0 +1,17 @@
+package com.neetcode.two_pointers;
+
+public class RemoveDuplicateFromSortedArray {
+
+  public int removeDuplicates(int[] nums) {
+    if (nums.length <= 1) return nums.length;
+    int i = 0, j = 1; // slow and fast pointers
+    while (j < nums.length) {
+      if (nums[i] != nums[j]) { // we ignore if equal => i is a marker of unique numbers in arr
+        i++; // increment by 1 to replace the duplicates
+        nums[i] = nums[j];
+      }
+      j++;
+    }
+    return i + 1;
+  }
+}
