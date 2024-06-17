a9e985f227afc9a46c361118dfb839b448648dfc
ilinhph <linhphanhust@gmail.com>
2024-06-17 17:10:01 +0700

    is palindrome best solution

--- itlinhph/two_pointers/is_palindrome.py
+++ itlinhph/two_pointers/is_palindrome.py
@@ -1,17 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # standalize string
-        new_s = ""
-        for char in s:
-            if char.isalpha() or (ord('0') <= ord(char) <= ord('9')):
-                new_s += char.lower()
         # use two pointers to validate string
-        right = len(new_s) - 1
-        for left in range(0, len(new_s)//2):
-            if new_s[left] != new_s[right]:
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
                 return False
-            left += 1
-            right -=1
+            left  += 1
+            right -= 1
 
         return True
 
@@ -20,7 +19,7 @@ if __name__ == "__main__":
     slist = ["Was it a car or a cat I saw", "tab a cat", "A man, a plan, a canal: Panama", "0P"]
 
     sol = Solution()
-    print(sol.isPalindrome(slist[3]))
     assert sol.isPalindrome(slist[0]) is True
     assert sol.isPalindrome(slist[1]) is False
     assert sol.isPalindrome(slist[2]) is True
+    assert sol.isPalindrome(slist[3]) is False
