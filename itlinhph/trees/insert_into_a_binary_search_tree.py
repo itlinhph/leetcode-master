29700a94b9c3587e54c8ef3c48c8394edd3c591a
ilinhph <linhphanhust@gmail.com>
2024-08-02 16:45:35 +0700

    add insert a binary search tree

---
+++ itlinhph/trees/insert_into_a_binary_search_tree.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/insert-into-a-binary-search-tree/description/
+from utils import TreeNode
+
+class Solution(object):
+    def insertIntoBST(self, root: TreeNode, val: int):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root.val:
+            return TreeNode(val)
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        else:
+            root.left = self.insertIntoBST(root.left, val)
+        
+        return root
+    
+    def insertIntoBST_2(self, root: TreeNode, val: int):
+        cur = root
+        while True:
+            if val > cur.val:
+                if cur.right:
+                    cur = cur.right
+                else:
+                    cur.right = TreeNode(val=val)
+                    break
+            else:
+                if cur.left:
+                    cur = cur.left
+                else:
+                    cur.left = TreeNode(val=val)
+                    break
+
+        return root
